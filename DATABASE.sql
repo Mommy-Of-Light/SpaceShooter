baed60a6c3784349a39a40720de7464ad3790442
Mommy-Of-Light <empress.mommy.of.light@gmail.com>
2026-09-21 15:42:04 +0200

    Create DATABASE.sql

---
+++ DATABASE.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS SpaceShooter;
+CREATE DATABASE SpaceShooter;
+
+USE SpaceShooter;
+
+CREATE TABLE scores
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pseudo VARCHAR(50) NOT NULL,
+    score INT NOT NULL,
+    difficulty VARCHAR(10) NOT NULL,
+    created_at DATETIME NOT NULL
+);
